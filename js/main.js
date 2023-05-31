d161a1346a752113d4e4e2ad31fbb443c277280c
Rasindu Hansika <rasinduhansika17218@gmail.com>
2023-05-31 12:24:16 +0530

    javascript part 1 done

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let runningTotal =0;
+let buffer = '0';
+let previousoperator ;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else handleNumber(value);
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    switch (symbol){
+        case 'C':
+            buffer ='0';
+            runningTotal = 0;
+            break;
+        case '←' :
+           if(buffer.length === 1) buffer = '0';
+           else buffer = buffer.substring(0,buffer.length-1);
+           break;
+        case '=':
+            if(previousoperator===null) {return}
+            flushOperation (parseInt(buffer));
+            buffer=runningTotal;
+            runningTotal=0;
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':    
+            Math(symbol);
+            break;
+    }
+}
+
+
